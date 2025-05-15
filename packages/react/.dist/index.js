26bb4cafc26d39b742d496e537ed5bbe9e48995d
theara <theara2048@gmail.com>
2025-05-15 16:37:06 +0700

    test first component

---
+++ packages/react/.dist/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Main() {
+    return /*#__PURE__*/ React.createElement("div", null, "hello world");
+}
+
+export { Main as default };
---
+++ packages/react/src/stories/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Component from "../../dist";
+
+export function Button() {
+  return <Component />;
+}
