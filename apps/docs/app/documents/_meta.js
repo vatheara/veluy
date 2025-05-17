17b2559352fc966b3e12042598274775ab2ce1bd
kong44 <sokkongchan2@gmail.com>
2025-05-17 16:25:53 +0700

    add missing doc getstart

--- apps/docs/app/documents/_meta.js
+++ apps/docs/app/documents/_meta.js
@@ -3,6 +3,10 @@ export default {
         title: "Introduction",
         type: 'doc'
     },
+    getting_start: {
+        title: "Getting Started",
+        type: 'doc'
+    },
     backend_adapters: {
         title: "Backend Adapters",
         type: 'doc'
