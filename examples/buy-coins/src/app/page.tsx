2f5482637258e36e8a5a713e14f0413122f83dbc
theara <theara2048@gmail.com>
2025-05-27 23:07:52 +0700

    fix build

--- examples/buy-coins/src/app/page.tsx
+++ examples/buy-coins/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@repo/ui/components/button";
 import { KhqrDialog, useKhqr } from "@weluy/react";
 
 export default function Home() {
-  const { isOpen, title, description, onConfirm, onCancel, setIsOpen, sessionTime, expired, setSessionTime, setExpired } = useKhqr();
+  const { setIsOpen } = useKhqr();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
