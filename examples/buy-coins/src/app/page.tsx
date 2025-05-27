8338246e138c7d9dd2b28494ab3561c13d7d0628
theara <theara2048@gmail.com>
2025-05-27 22:55:19 +0700

    wip example

--- examples/buy-coins/src/app/page.tsx
+++ examples/buy-coins/src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
+import { KhqrDialog, useKhqr } from "@veluy/react";
 
 export default function Home() {
+  const { setIsOpen, isOpen } = useKhqr();
+  console.log("Dialog isOpen:", isOpen);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <KhqrDialog />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -26,11 +31,12 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={() => {
+              console.log("Button clicked, opening dialog");
+              setIsOpen(true);
+            }}
           >
             <Image
               className="dark:invert"
@@ -39,8 +45,8 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+           Open Khqr Dialog
+          </button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
